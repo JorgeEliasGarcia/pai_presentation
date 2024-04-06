acefc64a6fb701c3095b1a0df1850db3b6d6cf00
usuario <alu0101472294@ull.edu.es>
2024-04-06 12:57:31 +0000

    la lia y pie chart terminado

--- la-liga/la-liga.js
+++ la-liga/la-liga.js
@@ -29,21 +29,21 @@ var dataSource = {
         plotToolText: '<div style=\'font-weight:bold; background-color:lightblue; padding:10px; border-radius:5px; color:black;\'> <b>$label</b>: $value puntos </div>',
     },
     data: [
-        { label: 'Barcelona', value: '90', color: '#fc0d1b,#fbb034', link: 'https://i.pinimg.com/736x/d4/d9/25/d4d9258a4e8fd626b4c72e177d10daef.jpg' }, // Azul y grana
-        { label: 'Real Madrid', value: '75', color: '#ffffff,#0071ce' }, // Blanco y azul
-        { label: 'Atlético de Madrid', value: '73', color: '#cb3524,#ffffff' }, // Rojo y blanco
-        { label: 'Valencia', value: '65', color: '#f7b020,#7b4f9d' }, // Naranja y morado
-        { label: 'Sevilla', value: '63', color: '#cf0720,#ffffff', link: 'https://sevillafc.es/' }, // Rojo y blanco
-        { label: 'Real Sociedad', value: '58', color: '#0071ce,#ffffff' }, // Azul y blanco
-        { label: 'Villarreal', value: '56', color: '#ffcd03,#00529f' }, // Amarillo y azul
-        { label: 'Athletic Club', value: '53', color: 'cb3524,#ffffff' }, // rojo y blanco
-        { label: 'Celta de Vigo', value: '50', color: '#4a85d8,#ffffff' }, // Celeste y blanco
-        { label: 'Getafe', value: '48', color: '#0090d1,#ffffff' }, // Azul y blanco
-        { label: 'Levante', value: '45', color: '#fc0d1b,#fbb034' }, // Azul y grana
-        { label: 'Espanyol', value: '42', color: '#009dff,#ffffff' }, // Azul y blanco
-        { label: 'Betis', value: '41', color: '#00a95c,#ffffff' }, // Verde y blanco
-        { label: 'Alavés', value: '38', color: '#000000,#ffffff' }, // Negro y blanco
-        { label: 'Mallorca', value: '36', color: '#E50000,#000000' } // Rojo y negro
+        { label: 'Barcelona', value: '90', color: '#fc0d1b,#fbb034', link: 'https://i.pinimg.com/736x/d4/d9/25/d4d9258a4e8fd626b4c72e177d10daef.jpg' },
+        { label: 'Real Madrid', value: '75', color: '#ffffff,#0071ce' },
+        { label: 'Atlético de Madrid', value: '73', color: '#cb3524,#ffffff' },
+        { label: 'Valencia', value: '65', color: '#f7b020,#7b4f9d' },
+        { label: 'Sevilla', value: '63', color: '#cf0720,#ffffff', link: 'https://sevillafc.es/' },
+        { label: 'Real Sociedad', value: '58', color: '#0071ce,#ffffff' },
+        { label: 'Villarreal', value: '56', color: '#ffcd03,#00529f' },
+        { label: 'Athletic Club', value: '53', color: 'cb3524,#ffffff' },
+        { label: 'Celta de Vigo', value: '50', color: '#4a85d8,#ffffff' },
+        { label: 'Getafe', value: '48', color: '#0090d1,#ffffff' },
+        { label: 'Levante', value: '45', color: '#fc0d1b,#fbb034' },
+        { label: 'Espanyol', value: '42', color: '#009dff,#ffffff' },
+        { label: 'Betis', value: '41', color: '#00a95c,#ffffff' },
+        { label: 'Alavés', value: '38', color: '#000000,#ffffff' },
+        { label: 'Mallorca', value: '36', color: '#E50000,#000000' }
     ]
 };
 // Render the graph. 
